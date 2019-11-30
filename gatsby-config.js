6e35e9ce465dea2faf6884630b9506a34b9ae62e
Alexey Shmalko <rasen.dubi@gmail.com>
2019-11-30 18:15:27 +0200

    Support draft posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
       options: {
         path: `${__dirname}/content/blog`,
         name: `blog`,
+        ignore: process.env.NODE_ENV === 'production' && ['**/_*'],
       },
     },
     {
